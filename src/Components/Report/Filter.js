21649245d4cb7c523e375d77971f25970b20918c
Vlad <vladislavholovko@gmail.com>
2018-04-04 12:34:12 +0300

    Add export xls/csv

--- src/Components/Report/Filter.js
+++ src/Components/Report/Filter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from 'react-redux';
+import XlsExport from 'xlsexport';
 //------------
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
@@ -24,6 +25,7 @@ class Filter extends React.Component {
     componentDidMount() {
         usReport.ReportFilter()
     }
+
 //-----------------------------
     searchReports() {
         let timeFrom = Object.keys(this.state.timeFrom).length === 0 && this.state.timeFrom.constructor === Object ?
@@ -61,7 +63,7 @@ class Filter extends React.Component {
                 return val.user_id === this.state.id && timeFrom <= dateFormat(val.date, "yyyy-mm-dd HH:MM");
             });
             usReport.ReportFilter(rList);
-        } else  if (this.state.id !== "all" && timeFrom !== null && timeTo !== null) {
+        } else if (this.state.id !== "all" && timeFrom !== null && timeTo !== null) {
             let rList = this.props.reports.filter((val) => {
                 return val.user_id === this.state.id && timeFrom <= dateFormat(val.date, "yyyy-mm-dd HH:MM") && timeTo >= dateFormat(val.date, "yyyy-mm-dd HH:MM");
             });
@@ -75,8 +77,52 @@ class Filter extends React.Component {
     }
 
     clearFilter() {
-        this.setState({ id: "all", timeFrom: {}, timeTo: {} });
+        this.setState({id: "all", timeFrom: {}, timeTo: {}});
         usReport.ReportFilter(undefined);
+    }
+
+//-----------------------------
+    userName(user_id) {
+        let user = this.props.users.find(value => {
+            return value._id === user_id
+        });
+        if (user === undefined) {
+            return null;
+        } else {
+            return user.fullName;
+        }
+    }
+
+    userEmail(user_id) {
+        let user = this.props.users.find(value => {
+            return value._id === user_id
+        });
+        if (user === undefined) {
+            return null;
+        } else {
+            return user.email;
+        }
+    }
+
+    saveExcel(typeFormat) {
+        let allList = this.props.reportFilter === undefined ? this.props.reports : this.props.reportFilter;
+        let reports = [["Name", "Email", "ID", "Approved"]];
+
+        for (let i = 0; i < allList.length; i++) {
+            let name = allList.map(value => this.userName(value.user_id));
+            let email = allList.map(value => this.userEmail(value.user_id));
+            let reportId = allList.map(value => value._id);
+            let reportApproved = allList.map(value => value.approved);
+            reports.push([name[i], email[i], reportId[i], reportApproved[i]])
+        }
+        let xls = new XlsExport(reports, "Reports");
+
+        if (typeFormat === "xls") {
+            xls.exportToXLS('reports.xls');
+        } else if (typeFormat === "csv") {
+            xls.exportToCSV('reports.csv');
+        }
+
     }
 
 //-----------------------------
@@ -129,13 +175,14 @@ class Filter extends React.Component {
                         <FlatButton backgroundColor="rgba(142, 141, 141, 0.2)"
                                     hoverColor="rgba(142, 141, 141, 0.5)"
                                     icon={<i className="material-icons">file_download</i>} label="XLS"
-                                    onClick={()=> usReport.saveXls()}
+                                    onClick={() => this.saveExcel("xls")}
                         />
                     </div>
                     <div style={{margin: "2px 0"}}>
                         <FlatButton backgroundColor="rgba(142, 141, 141, 0.2)"
                                     hoverColor="rgba(142, 141, 141, 0.5)"
                                     icon={<i className="material-icons">file_download</i>} label="CSV"
+                                    onClick={() => this.saveExcel("csv")}
                         />
                     </div>
                 </div>
@@ -144,4 +191,8 @@ class Filter extends React.Component {
     }
 }
 
-export default connect(store => ({reports: store.report_info, users: store.user_info}))(Filter)
+export default connect(store => ({
+    reports: store.report_info,
+    reportFilter: store.filter_list,
+    users: store.user_info
+}))(Filter)
